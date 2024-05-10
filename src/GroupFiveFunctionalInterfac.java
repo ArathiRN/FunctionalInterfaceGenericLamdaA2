00946ab05a18f9de6bdb55b7d740222a3adb91ec
ArathiRN <arathirn@gmail.com>
2024-05-10 10:16:04 -0700

    Java program that uses lambda expressions with a generic type functional interface

---
+++ src/GroupFiveFunctionalInterfac.java
@@ -0,0 +1,4 @@
+public interface GroupFiveFunctionalInterfac <T,R>{
+    //String str
+    R transform(T str);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+/*
+Create a Java program that uses lambda expressions with a generic
+type functional interface. Define a functional interface:
+GroupOneFunctionalInterface<T, R> with a single abstract method
+‘transform’ with one type param and a return type. Use a lambda
+expression to implement the functional interface. The lambda
+expression takes a String input and returns its length as an Integer.
+Create an instance of the functional interface ‘transformer’ and use it to
+transform a string to its length and then display the length of the input
+string.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        GroupFiveFunctionalInterfac <String, Integer> Obj = (str)-> str.length();
+        System.out.println(Obj.transform("String length"));
     }
 }
